1612fd1506c47e94bd648ab5b649973017d8fe4a
Sarah <smkb17@gmail.com>
2020-09-02 15:09:21 -0500

    jobRole input works
    when a user selects the other option it shows an text box

--- interactive-form-v2/js/script.js
+++ interactive-form-v2/js/script.js
@@ -1,2 +1,15 @@
-const setFocus = document.getElementById('name').focus;
-document.write(setFocus);
+const focusText = document.getElementById('name').focus();
+const input= document.getElementById('other-title');
+const jobTitle= document.getElementById('title');
+
+//hiding the input initially
+input.style.display= 'none';
+
+//if they select other then the input will appear
+jobTitle.addEventListener('change', (e) => {
+  if (e.target.value === 'other') {
+       input.style.display = 'block';
+   }else{
+       input.style.display = 'none';
+   }
+});
